c07e9d6fabea9e04097d950600439b1c117f98b2
hgytryhrtyeyh <udnisas@gmail.com>
2023-03-16 00:49:37 -0700

    database function changers

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -12,7 +12,7 @@ test('Testing add students', async t => {
     await t.typeText("#student-id", "999999");
     await t.typeText("#student-name", "Pasindu Basnayaka");
     await t.typeText("#student-age", "45");
-    await t.typeText("#student-Hometown", "Kandy");
+    await t.typeText("#student-Hometown", "Catholic");
     await t.click("#student-add");
 
     await t.navigateTo("/student");
