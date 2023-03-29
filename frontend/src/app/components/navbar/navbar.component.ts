edddfeebd2799fe260d867a8b79131c42115439b
hgytryhrtyeyh <udnisas@gmail.com>
2023-03-29 13:33:13 -0700

    database function changers

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
